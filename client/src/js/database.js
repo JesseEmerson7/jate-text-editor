d318d0af06ef6887a54b01abf4c0ebdf8b0915b3
Jesse Emerson <jesseemerson7@gmail.com>
2023-05-25 19:31:36 -0400

    added install button func

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// Method that takes some content and adds it to the IndexedDB database using the idb module
+// adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
   console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
@@ -20,10 +20,10 @@ export const putDb = async (content) => {
   const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result.value);
+  console.log("data saved to the database", result.value);
 };
 
-// Method that gets content from the IndexedDB database using the idb module
+// gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
   console.log("GET from the database");
   const jateDb = await openDB("jate", 1);
@@ -32,9 +32,8 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   result
-    ? console.log("🚀 - data retrieved from the database", result.value)
-    : console.log("🚀 - data not found in the database");
-  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+    ? console.log("data retrieved from the database", result.value)
+    : console.log("data not found in the database");
   return result?.value;
 };
 
